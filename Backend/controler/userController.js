2fcef4132f1ca0b2192bd70cf76c9df3c935fe17
Tongmean Teang <tongmean.teang@wdc.com>
2022-09-29 10:10:38 +0700

    login add

--- Backend/controler/userController.js
+++ Backend/controler/userController.js
@@ -1,12 +1,45 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+//Token Function
+const createToken = (_id) =>{
+    return jwt.sign({_id: _id}, process.env.SECRET, {expiresIn: '3d'})
+}
+
 //Loginuser
 const loginUser = async (req, res) =>{
-    res.json({"meg":"login"})
+    const {email, password } = req.body
+
+    try{
+        const user = await User.login(email, password)
+
+        //Create Token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+
+    }
+
 }
 
 //SignupUser
 const SignupUser = async (req, res) =>{
-    res.json({"meg":"signup"})
+    const {email, password } = req.body
+
+    try{
+        const user = await User.signup(email, password)
+        // Create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
 
 }
 
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose')
 //INvoke Schima
 const Schema = mongoose.Schema
-
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 const userSchema = new Schema({
     email:{
         type: String,
@@ -15,5 +16,55 @@ const userSchema = new Schema({
         
     }
 }, {timestamps: true})
+// Static method signup
+userSchema.statics.signup = async function(email, password) {
+    //Validation
+    if(!email || !password){
+        throw Error('All field must be fill')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email not Valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if(exists){
+        throw Error('Email already Exist')
+    }
+    // For add prefix to the same password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+
+}
+
+//static method login
+userSchema.statics.login = async function(email, password){
+
+    if (!email || !password){
+        throw Error('All Fields must be Fill')
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect Password')
+    }
+    console.log(email, password)
+
+    return user
+}
 
 module.exports = mongoose.model('User',userSchema)
